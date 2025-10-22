1e4fec2f44c729dcde1353e41ad8d7b644553673
Zahid <zahidabbas110@gmail.com>
2025-10-22 15:00:41 +0530

    Added one utility method.

--- src/main/java/com/app/zahid/Dependency/AppUtil.java
+++ src/main/java/com/app/zahid/Dependency/AppUtil.java
@@ -1,7 +1,13 @@
 package com.app.zahid.Dependency;
 
+import java.util.List;
+
 public class AppUtil {
    private  AppUtil() {
+   }
 
+
+    public String convertToString(List<String> list) {
+       return String.join(",", list);
     }
 }

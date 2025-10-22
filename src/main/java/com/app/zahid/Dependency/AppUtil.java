02e34c73e66cdfa762af531f73a3a0a1aa0304d3
zahidabbas4787 <zahidabbas4787@gmail.com>
2025-10-22 15:04:08 +0530

    Update AppUtil.java

--- src/main/java/com/app/zahid/Dependency/AppUtil.java
+++ src/main/java/com/app/zahid/Dependency/AppUtil.java
@@ -10,4 +10,8 @@ public class AppUtil {
     public String convertToString(List<String> list) {
        return String.join(",", list);
     }
+
+    public String convertToString(List<String> list, String delimeter) {
+       return String.join(delimeter, list);
+    }
 }
